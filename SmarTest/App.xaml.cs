050dd1fadc714aa91ae8b41c0abd5b800652b6c8
Andrei Beres <andrei.beres@yahoo.com>
2024-11-03 18:13:47 +0200

    Added initial project files for the .NET MAUI (With Hybrid Blazor) Project

---
+++ SmarTest/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace SmarTest
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
