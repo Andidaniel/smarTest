539aa789e45576f098817b86b935ce9f2168e421
Andrei Beres <andrei.beres@yahoo.com>
2024-12-07 18:26:56 +0200

    Added base classes

---
+++ SmarTest/DataLayer/Models/StudentClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmarTest.DataLayer.Models
+{
+    public class StudentClass
+    {
+        public int Id { get; set; }
+        public int Number { get; set; }
+        public string Subclass { get; set; }
+        public string ClassMaster { get; set; }
+    }
+}
